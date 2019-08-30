febe1900e69f3aba35eece75956440186f3ac865
Administrator <Administrator@192.168.1.206>
2019-08-30 21:50:20 +0800

    commit

--- src/main/java/com/example/myBlog/Mapper/IUser.java
+++ src/main/java/com/example/myBlog/Mapper/IUser.java
@@ -15,13 +15,14 @@ public interface IUser {
 	@Select("select * from myuser ")
 	public List<myUser> findAllUser();
 	
-	@Select("select * from myuser where id=${id}")
+	@Select("select * from myuser where id=#{id}")
 	public myUser findUserByID(@Param("id")int id);
 	
 	@Select("select * from tab ")
 	public List<String> findAllTable();
 	
-	@Insert("insert into myuser(id,name,address,age) values(${user.id},${user.name},${user.address},${user.age})")
+	@Insert("insert into myuser(id,name,address,age) "+
+			"values(#{user.id},#{user.name},#{user.address},#{user.age})")
 	public void insert(@Param("user")myUser user);
 	
 	
