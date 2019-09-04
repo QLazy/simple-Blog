742b6541838db6cd8c884a97d89c528db7ceb6d7
QLazy <1069932186@qq.com>
2019-09-04 22:08:15 +0800

    commit

--- src/main/java/com/example/myBlog/controller/publishController.java
+++ src/main/java/com/example/myBlog/controller/publishController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -34,13 +35,26 @@ public class publishController {
 			@RequestParam("title") String title,
 			@RequestParam("description") String description,
 			@RequestParam("tag") String tag,
-			HttpServletRequest request ) {
+			HttpServletRequest request,
+			Model model) {
 		
 		
 		myUser user = myservice.queryUserByToken(request);
 		
-		
 		myQuestion mq = new myQuestion();
+		
+		if(title==null || title.equals("")) {
+			model.addAttribute("error", "标题不能为空");
+			return "publish";
+		}
+		if(tag==null || tag.equals("")) {
+			model.addAttribute("error", "标签不能为空");
+			return "publish";
+		}
+		if(description==null || description.equals("")) {
+			model.addAttribute("error", "问题补充不能为空");
+			return "publish";
+		}
 		
 		mq.setTitle(title);
 		mq.setTag(tag);
