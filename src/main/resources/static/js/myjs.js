c21ee88a05cbac0ff2b8beba847026365072e39f
QLazy <1069932186@qq.com>
2019-09-16 21:53:50 +0800

    commit

--- src/main/resources/static/js/myjs.js
+++ src/main/resources/static/js/myjs.js
@@ -22,21 +22,32 @@
 
 function post(){
 	var id = $("#question_id").val();
-	var content = $("#comment_content").val();
+	var content = $("#commentContent").val();
 	var jsonData = {"parentId":id,"parentType":1,"content":content};
-	alert(id+"+++++"+content);
 	$.ajax({
 		type : "POST",
 		contentType : "application/json;charset=utf-8",
 		url : "/comment",
 		data : JSON.stringify(jsonData),
 		dataType: "json",
-		success : function(aaa) {
-			alert("success" + aaa);
+		success : function(parm) {
+			if(parm.code == 200){
+				// 将输入框隐藏或缩小
+				$("#commentUser").hide();
+				$("#commentSubmit").hide();
+				$("#commentContent").attr("rows",1);
+				
+			}else if(parm.code == 2003){
+				var isAccepted = confirm(parm.message);
+				if(isAccepted){
+					//window.localtion.replace("登录页面");
+					//window.localStorage.setItem("closeFlag",true);
+					alert(parm.message);
+				}else{
+					alert(parm.message);
+				}
+			}
 		},
-		error : function(aaa) {
-			alert("error" + aaa);
-		}
 	});
 }
 
