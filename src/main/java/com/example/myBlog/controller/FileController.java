195170e7ea0a481865bfd0f8fe0826adffe6da00
QLazy <1069932186@qq.com>
2019-09-24 22:01:08 +0800

    增加使用SDK上传图片到云端

--- src/main/java/com/example/myBlog/controller/FileController.java
+++ src/main/java/com/example/myBlog/controller/FileController.java
@@ -1,16 +1,38 @@
 package com.example.myBlog.controller;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.example.myBlog.dto.FileDTO;
+import com.example.myBlog.provider.UCloudProvider;
 
 @Controller
 public class FileController {
+
+	@Autowired
+	private UCloudProvider uCloudProvider;
+
 	@ResponseBody
 	@RequestMapping("/file/upload")
-	public FileDTO upload() {
+	public FileDTO upload(HttpServletRequest request) {
+
+		MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+		MultipartFile file = multipartRequest.getFile("editormd-image-file");
+
+		try {
+			uCloudProvider.upload(file.getInputStream(), file.getContentType(), file.getOriginalFilename());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 		FileDTO fileDTO = new FileDTO();
 		fileDTO.setSuccess(1);
 		fileDTO.setUrl("/images/1.jpg");
