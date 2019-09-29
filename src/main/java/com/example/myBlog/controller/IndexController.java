5698c1f906f9d271191e7be941126b5d2d9424c4
QLazy <1069932186@qq.com>
2019-09-29 13:28:16 +0800

    修复BUG

--- src/main/java/com/example/myBlog/controller/IndexController.java
+++ src/main/java/com/example/myBlog/controller/IndexController.java
@@ -5,11 +5,19 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.example.myBlog.dto.CommentCreatorDTO;
 import com.example.myBlog.dto.PaginationDTO;
 import com.example.myBlog.dto.QuestionQueryDTO;
+import com.example.myBlog.dto.ResultDTO;
+import com.example.myBlog.entity.MyComment;
+import com.example.myBlog.entity.MyUser;
+import com.example.myBlog.excuption.CustomizeErrorCode;
 import com.example.myBlog.service.QuestionService;
 
 @Controller
@@ -33,5 +41,12 @@ public class IndexController {
 		model.addAttribute("pagination", pagination);
 
 		return "index";
+	}
+	
+	@SuppressWarnings("rawtypes")
+	@ResponseBody
+	@RequestMapping(path = "/time", method = RequestMethod.POST)
+	public ResultDTO like() {
+		return ResultDTO.succesOf("1569733036935");
 	}
 }
